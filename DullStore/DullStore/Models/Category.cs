665d50151e1f68a206a0406ea1d9333d3dadeb40
NamThan02 <namn54589@gmail.com>
2023-09-11 16:59:06 +0700

    project 1

---
+++ DullStore/DullStore/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DullStore.Models
+{
+    public class Category
+    {
+        public List<string> listdm { get; set; }
+        public Category()
+        {
+            listdm = new List<string>();
+            listdm.Add("Lacoste ");
+            listdm.Add("Canifa");
+            listdm.Add("Aristino ");
+            listdm.Add("Biluxury");
+            listdm.Add("polo Ralph Lauren");
+        }
+    }
+}
---
+++ DullStore/DullStore/Models/DanhMucModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DullStore.Models
+{
+    public class DanhMucModel
+    {
+        public List<SanPhamModel> ListLacoste { get; set; }
+
+        public List<SanPhamModel> ListCanifa { get; set; }
+
+        public List<SanPhamModel> ListAristino { get; set; }
+
+        public List<SanPhamModel> ListBiluxury { get; set; }
+
+        public List<SanPhamModel> ListpoloRalphLauren { get; set; }
+
+        public List<SanPhamModel> D1 { get; set; }
+        public List<SanPhamModel> D2 { get; set; }
+        public List<SanPhamModel> D3 { get; set; }
+
+        public DanhMucModel()
+        {
+            ListLacoste = new List<SanPhamModel>();
+            ListLacoste.Add(new SanPhamModel("/Content/Image/Lacoste_1.jpg"));
+            ListLacoste.Add(new SanPhamModel("/Content/Image/Lacoste_2.jpg"));
+            ListLacoste.Add(new SanPhamModel("/Content/Image/Lacoste_3.jpg"));
+
+            ListCanifa = new List<SanPhamModel>();
+            ListCanifa.Add(new SanPhamModel("/Content/Image/Canifa_1.jpg"));
+            ListCanifa.Add(new SanPhamModel("/Content/Image/Canifa_2.jpg"));
+
+            ListAristino = new List<SanPhamModel>();
+            ListAristino.Add(new SanPhamModel("/Content/Image/Aristino_1.jpg"));
+
+            ListBiluxury = new List<SanPhamModel>();
+            ListBiluxury.Add(new SanPhamModel("/Content/Image/Biluxury.jpg"));
+
+            ListpoloRalphLauren = new List<SanPhamModel>();
+            ListpoloRalphLauren.Add(new SanPhamModel("/Content/Image/poloRalphLauren_1.jpg"));
+            ListpoloRalphLauren.Add(new SanPhamModel("/Content/Image/poloRalphLauren_2.jpg"));
+
+            D1 = new List<SanPhamModel>();
+            D1.Add(new SanPhamModel("/Content/Image/Lacoste_1.jpg"));
+            D1.Add(new SanPhamModel("/Content/Image/Lacoste_2.jpg"));
+            D1.Add(new SanPhamModel("/Content/Image/Lacoste_3.jpg"));
+
+            D2 = new List<SanPhamModel>();
+            D2.Add(new SanPhamModel("/Content/Image/Canifa_1.jpg"));
+            D2.Add(new SanPhamModel("/Content/Image/Canifa_2.jpg"));
+            D2.Add(new SanPhamModel("/Content/Image/Aristino_1.jpg"));
+
+            D3 = new List<SanPhamModel>();
+            D3.Add(new SanPhamModel("/Content/Image/Biluxury.jpg"));
+            D3.Add(new SanPhamModel("/Content/Image/poloRalphLauren_1.jpg"));
+            D3.Add(new SanPhamModel("/Content/Image/poloRalphLauren_2.jpg"));
+        }
+    }
+}
